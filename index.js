e3d8c66a474ed404bb29b5360c980de2453e07e9
stufonn-cell <stufonn@gmail.com>
2025-10-27 06:53:22 -0500

    Add: Tutorial 07

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 
+const os = require('os');
+
 const app = express()
 
 const port = 80
@@ -16,7 +18,7 @@ const phrases = [
 
 app.get('/', (req, res) => {
   const number = Math.floor(Math.random() * 7);
-  res.send(phrases[number])
+  res.send(phrases[number]+" - Container Id: "+os.hostname());
 })
 
 app.listen(port, () => {
